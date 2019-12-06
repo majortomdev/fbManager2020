c368bfa681ae74c16ed14581cc844e5bf27e5874
Joseph Kinahan <kinahanj25@gmail.com>
2019-12-06 21:16:31 +0000

    added method to locate player from id

--- src/main/java/com/kinadev/FbManager2020/controllers/FbController.java
+++ src/main/java/com/kinadev/FbManager2020/controllers/FbController.java
@@ -1,12 +1,15 @@
 package com.kinadev.FbManager2020.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,16 +26,32 @@ public class FbController {
 	@Autowired
 	private FbRepository fbRepo;
 	
-	@GetMapping("players")
-	public List<Player> getAllPlayers() {
-		return (List<Player>) fbRepo.findAll();
+	/*
+	 * @GetMapping("/players") public List<Player> getAllPlayers() { return
+	 * (List<Player>) fbRepo.findAll(); }
+	 */
+	
+	@GetMapping("/players")
+	public ResponseEntity<Iterable<Player>>  getAllPlayers() {
+		Iterable<Player> players = fbRepo.findAll();
+		
+		return ResponseEntity.ok().body(players);
 	}
 	
-	@PostMapping(value = "players", consumes = "application/json", 
-			 produces = "application/json")
-	public Player createPlayer(@Valid @RequestBody Player player) {
-		return fbRepo.save(player);
-	//	return player;
+	@GetMapping("/players/{id}")
+	public ResponseEntity<Optional<Player>>  getPlayerById(@PathVariable(value= "id") Long playerId) {
+			//throws ResourceNotFoundException {
+		Optional<Player> player = fbRepo.findById(playerId);
+//				.orElseThrow(() -> 
+//		ResourceNotFoundException("player not found for id :: "+playerId));
+		return ResponseEntity.ok().body(player);
 	}
+	
+//	@PostMapping(value = "players", consumes = "application/json", 
+//			 produces = "application/json")
+//	public Player createPlayer(@Valid @RequestBody Player player) {
+//		return fbRepo.save(player);
+//	//	return player;
+//	}
 
 }
